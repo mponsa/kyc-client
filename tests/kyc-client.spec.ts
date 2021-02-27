d4e3d4673ba93078d2fa0e2a56122bd775eb5413
mponsa-meli <manuel.ponsa@mercadolibre.com>
2021-02-27 18:26:14 -0300

    Added tests to cover the new logic

--- tests/kyc-client.spec.ts
+++ tests/kyc-client.spec.ts
@@ -1,9 +1,20 @@
+
+
 import KYC from '../lib/kyc-client';
 import * as errors from '../lib/errors/errors';
+import axios from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
 
 
 describe("KYC Client test", () => {
-    it("Should pass validation with a valid Payload", async () => {
+    it("Should return true validation with a valid Payload", async () => {
         const payload = {
             birthDate : "1985-02-08",
             givenName : "James",
@@ -14,11 +25,49 @@ describe("KYC Client test", () => {
             expiryDate : "2020-01-01"
         }
 
+        mockedAxios.post.mockResolvedValue({
+            "data":{
+                "verifyDocumentResult": {
+                    "type": "DriverLicenceResponse"
+                    },
+                    "verificationRequestNumber": 12345,
+                    "verificationResultCode": "Y"
+                }
+            })
+
         const client = new KYC('testKey')
 
         const res = await client.validate(payload)
 
         expect(res.kycResult).toBe(true)
+    })
+
+    it("Should return false validation with a valid Payload", async () => {
+        const payload = {
+            birthDate : "1985-02-08",
+            givenName : "James",
+            middleName : "Robert",
+            familyName : "Smith",
+            licenceNumber : "94977000",
+            stateOfIssue : "NSW",
+            expiryDate : "2020-01-01"
+        }
+
+        mockedAxios.post.mockResolvedValue({
+            "data":{
+                "verifyDocumentResult": {
+                    "type": "DriverLicenceResponse"
+                    },
+                    "verificationRequestNumber": 12345,
+                    "verificationResultCode": "N"
+                }
+            })
+
+        const client = new KYC('testKey')
+
+        const res = await client.validate(payload)
+
+        expect(res.kycResult).toBe(false)
     })
 
     it("Should fail with error indicating an invalid Payload", async () => {
@@ -27,6 +76,58 @@ describe("KYC Client test", () => {
             givenName : "James",
             middleName : "Robert"
         }
+
+        const client = new KYC('testKey')
+
+        await expect(client.validate(payload)).rejects.toThrow()
+    })
+
+    it("Should fail with error indicating a failed response", async () => {
+        const payload = {
+            birthDate : "1985-02-08",
+            givenName : "James",
+            middleName : "Robert",
+            familyName : "Smith",
+            licenceNumber : "94977000",
+            stateOfIssue : "NSW",
+            expiryDate : "2020-01-01"
+        }
+
+        mockedAxios.post.mockResolvedValue({
+            "data":{
+                "verifyDocumentResult": {
+                    "type": "DriverLicenceResponse"
+                    },
+                    "verificationRequestNumber": 12345,
+                    "verificationResultCode": "S"
+                }
+            })
+
+        const client = new KYC('testKey')
+
+        await expect(client.validate(payload)).rejects.toThrow()
+    })
+
+    it("Should fail with error indicating a failed response", async () => {
+        const payload = {
+            birthDate : "1985-02-08",
+            givenName : "James",
+            middleName : "Robert",
+            familyName : "Smith",
+            licenceNumber : "94977000",
+            stateOfIssue : "NSW",
+            expiryDate : "2020-01-01"
+        }
+
+        mockedAxios.post.mockResolvedValue({
+            "data":{
+                "verifyDocumentResult": {
+                    "type": "DriverLicenceResponse"
+                    },
+                    "verificationRequestNumber": 12345,
+                    "verificationResultCode": "D"
+                }
+            })
 
         const client = new KYC('testKey')
 
